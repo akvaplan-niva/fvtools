3bdd08521a1401a55f738c952cdb78a30685dfc2
Håvard Espenes <hes@akvaplan.niva.no>
2025-12-05 10:03:50 +0100

    grid object for GLORYS files (CMEMS 1/12 degree ocean model)

---
+++ fvtools/grid/glorys_grid.py
@@ -0,0 +1,67 @@
+import xarray as xr
+import numpy as np
+
+class GLORYS:
+    def __init__(self, ncglorys):
+        '''
+        Initialize grid variables for the GLORYS model
+        '''
+        # We read from the grid. Glorys data is gridded to z-levels, so we need to 
+        with xr.open_dataset(ncglorys) as df:
+            self.depth = df.depth.data # load depth vector, same for all grid cells
+            self.lon, self.lat = np.meshgrid(df.longitude.data, df.latitude.data) # meshgrid lon,lat
+            self.zlevels = np.tile(df.depth.data[:, None, None], (1, *self.lon.shape)) # make a depth matrix for all points
+            self.land_3d = np.isnan(df.so.isel(time = 0)) # Identify which gridpoints/depths do not cover the ocean
+            
+    @property
+    def depth_below_geoid(self):
+        '''
+        Water depth, depth below mean sea surface etc.
+        '''
+        dpt = np.copy(self.zlevels)
+        dpt[self.land_3d] = np.nan
+        return np.nanmax(dpt, axis = 0)
+
+    @property
+    def land_mask(self):
+        '''
+        Mask for land
+        - True where there is land
+        '''
+        return np.isnan(self.depth_below_geoid)
+    
+    @property
+    def lon_center(self):
+        '''
+        Longitude in the center of lat/lon cells
+        '''
+        return (self.lon[:-1,:-1] + self.lon[1:,1:])/2
+    
+    @property
+    def lat_center(self):
+        '''
+        Latitude in the center of lat/lon cells
+        '''
+        return (self.lat[:-1,:-1] + self.lat[1:,1:])/2
+    
+    @property
+    def lonlat_center(self):
+        '''
+        center coordinate as (n,2) array
+        '''
+        return np.array([self.lon_center.ravel(), self.lat_center.ravel()]).T
+    
+    @property
+    def lonlat_data(self):
+        '''
+        data coordinate as (n,2) array
+        '''
+        return np.array([self.lon.ravel(), self.lat.ravel()]).T
+    
+    # Aliases
+    @property
+    def h_rho(self):
+        '''
+        Use same name convention as ROMS so that we can use MatchTopo without too much fuss.
+        '''
+        return self.depth_below_geoid
